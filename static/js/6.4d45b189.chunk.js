28bf8da869482093cc8517a03cf09c126e4ec9cb
AIASolution <58142664+bestsolution4u@users.noreply.github.com>
2022-09-26 13:51:45 -0600

    init: initial commmit

---
+++ static/js/6.4d45b189.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[6],{339:function(n,o){},359:function(n,o){},365:function(n,o){},367:function(n,o){}}]);
+//# sourceMappingURL=6.4d45b189.chunk.js.map
---
+++ static/js/8.541cad3b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[8],{291:function(n,o){}}]);
+//# sourceMappingURL=8.541cad3b.chunk.js.map
